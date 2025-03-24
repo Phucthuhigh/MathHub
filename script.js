963643629ff52a68a85875e331f8806abe2b71df
phucthuhigh <60474809+Phucthuhigh@users.noreply.github.com>
2025-03-24 08:16:27 +0700

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+var currentX = '';
+var currentY = '';
+var movementConstant = .025;
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+$(".bubbles i").each(function(i, el) {
+    $(el).css({
+        "animation-duration": `calc(50s / ${getRndInteger(10, 30)})`
+    })
+})
